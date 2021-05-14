b64fddfca74c7e12edf09f212993eceec7933bc9
MikHail854 <vip.moneta95@mail.ru>
2021-05-14 15:16:44 +0700

    consuming-a-restful-web-service

---
+++ consuming-a-restful-web-srvice/src/test/java/com/example/consumingarestfulwebsrvice/ConsumingARestfulWebSrviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumingarestfulwebsrvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingARestfulWebSrviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
