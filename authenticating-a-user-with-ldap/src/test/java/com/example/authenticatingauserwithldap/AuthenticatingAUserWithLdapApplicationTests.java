b8fccb164c1a5b0aa34df5ae839ad424bb4d2669
MikHail854 <vip.moneta95@mail.ru>
2021-05-22 16:55:45 +0700

    Authenticating a User with LDAP

---
+++ authenticating-a-user-with-ldap/src/test/java/com/example/authenticatingauserwithldap/AuthenticatingAUserWithLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authenticatingauserwithldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatingAUserWithLdapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
